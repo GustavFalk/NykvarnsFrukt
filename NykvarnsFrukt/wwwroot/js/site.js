4462248abbe9adafcf52dc449737afe113f72270
Gustav Falk <gustav.falk@paytrim.com>
2023-05-03 23:52:11 +0200

    fixed nav with animations and burger menu

--- NykvarnsFrukt/wwwroot/js/site.js
+++ NykvarnsFrukt/wwwroot/js/site.js
@@ -3,4 +3,28 @@
 
 // Write your JavaScript code.
 
-//meny animation
+
+//Expand and collapse nav
+function expandCollapseNav() {
+    var x = document.getElementById("nav-dropdown");
+    if (x.classList.contains("nav-grow")) {
+
+        $("ul.nav-dropdown").removeClass("nav-grow");
+        $("ul.nav-dropdown").addClass("nav-collapse");
+       
+    } else {
+        $("ul.nav-dropdown").removeClass("nav-collapse");
+        $("ul.nav-dropdown").addClass("nav-grow");
+        
+    }
+}
+
+//set active menu option
+$(document).ready(function () {
+    $("ul.nav-dropdown> li").click(function (e) {
+        $("ul.nav-dropdown > li").removeClass("active");
+        $(this).addClass("active");
+    });
+});
+
+
