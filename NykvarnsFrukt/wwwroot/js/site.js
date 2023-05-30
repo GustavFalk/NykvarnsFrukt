185e190baff88e716991c242f0f88d16eb9dace3
Gustav Falk <gustav.falk@paytrim.com>
2023-05-30 23:12:37 +0200

    alt texts.  all pages done for phone. interactivity on buttons.

--- NykvarnsFrukt/wwwroot/js/site.js
+++ NykvarnsFrukt/wwwroot/js/site.js
@@ -11,11 +11,11 @@ function expandCollapseNav() {
 
         $("ul.nav-dropdown").removeClass("nav-grow");
         $("ul.nav-dropdown").addClass("nav-collapse");
-       
+
     } else {
         $("ul.nav-dropdown").removeClass("nav-collapse");
         $("ul.nav-dropdown").addClass("nav-grow");
-        
+
     }
 }
 
@@ -28,7 +28,7 @@ function expandProductNav() {
         $("h4#arrow-icon").removeClass("arrow-right");
         $("li.product-link").addClass("under-nav-grow");
         $("h4#arrow-icon").addClass("arrow-down");
-        
+
     }
     else {
         $("li.product-link").removeClass("under-nav-grow");
@@ -49,16 +49,6 @@ $(document).ready(function () {
     });
 });
 
-//set active menu option
-//$(document).ready(function () {
-//    $("ul.nav-dropdown> li").click(function (e) {
-//        $("ul.nav-dropdown > li").removeClass("active");
-//        $(this).addClass("active");
-//        if ((this).classList.contains("product-link")) {
-//            $("li.product-nav").addClass("active");
-//        }
-//    });
-//});
 var loc = window.location.pathname;
 var pageName = loc.replace("/", "");
 $("ul.nav-dropdown > li").removeClass("active");
@@ -67,12 +57,44 @@ $(activePageNav).addClass("active");
 
 
 //Open mail client
-document.getElementById("mail-btn").onclick = function () {
-    location.href = "mailto:gustavfalk@live.se";
-};
+
+
+var mailBtn = document.getElementById("mail-btn")
+if (mailBtn != null) {
+    mailBtn.onclick = function () {
+        location.href = "mailto:gustavfalk@live.se";
+    };
+}
+
+//Open mail client
+var mailBtnOrder = document.getElementById("mail-btn-order")
+if (mailBtnOrder != null) {
+    mailBtnOrder.onclick = function () {
+        location.href = "mailto:gustavfalk@live.se";
+    };
+}
+
+//Open phone
+var phoneBtn = document.getElementById("phone-btn")
+if (phoneBtn != null) {
+    phoneBtn.onclick = function () {
+        location.href = "tel:046105545684";
+    };
+}
 
 //Open map application/webpage
-document.getElementById("store-icon").onclick = function () {
-    window.open("http://maps.apple.com/?q=S%C3%B6dert%C3%A4ljev%C3%A4gen+20,+155+32+Nykvarn","_blank");
-};
+var storeIcon = document.getElementById("store-icon");
+if (storeIcon != null) {
+    storeIcon.onclick = function () {
+        window.open("http://maps.apple.com/?q=S%C3%B6dert%C3%A4ljev%C3%A4gen+20,+155+32+Nykvarn", "_blank");
+    };
+}
+
+//Open facebook
+var facebook = document.getElementById("facebook");
+if (facebook != null) {
+    facebook.onclick = function () {
+        window.open("https://www.facebook.com/nykvarnsfrukt", "_blank");
+    };
+}
 
